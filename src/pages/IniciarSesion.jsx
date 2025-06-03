b2f220f72609434b8220a8d574c47e6adc0a2558
JuanJose <draxx0405@gmail.com>
2025-06-03 11:37:26 -0500

    sdaf

--- src/pages/IniciarSesion.jsx
+++ src/pages/IniciarSesion.jsx
@@ -52,7 +52,7 @@ export default function Login() {
 
     const handleMessage = (event) => {
         // Asegúrate de que el mensaje provenga de un origen permitido
-        const allowedOrigins = ['http://localhost:8000', 'https://d10gzc4g-5173.usw3.devtunnels.ms/'];
+        const allowedOrigins = ['http://backend-itsa-avisos-production-ecc1.up.railway.app', 'https://frontend-itsa-avisos-production.up.railway.app/'];
         if (!allowedOrigins.includes(event.origin)) return;
 
         if (event.data.type === 'MSAL_AUTH' && event.data.token) {
